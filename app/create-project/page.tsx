aef1c88e3b61b947613f086a28ffd25d8c29a941
nikolamirilo <nikolamirilo@gmail.com>
2023-07-11 23:51:08 +0200

    started with modal

---
+++ app/create-project/page.tsx
@@ -0,0 +1,14 @@
+import Modal from "@/components/Modal";
+import ProjectForm from "@/components/ProjectForm";
+import React from "react";
+
+const CreateProject = () => {
+  return (
+    <Modal>
+      <h3 className="modal-head-text">Create new project</h3>
+      <ProjectForm />
+    </Modal>
+  );
+};
+
+export default CreateProject;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import AuthProviders from "@/components/AuthProviders";
 import { getCurrentUser } from "@/lib/session";
+import { signOut } from "next-auth/react";
+import ProfileMenu from "@/components/ProfileMenu";
 
 const Navbar = async () => {
   const session = await getCurrentUser();
@@ -23,15 +25,7 @@ const Navbar = async () => {
       <div className="flexCenter gap-4">
         {session?.user ? (
           <>
-            {session?.user?.image && (
-              <Image
-                src={session.user.image}
-                width={40}
-                height={40}
-                className="rounded-full"
-                alt={session.user.name}
-              />
-            )}
+            <ProfileMenu session={session} />
             <Link href="/create-project">Share Your Work</Link>
           </>
         ) : (
